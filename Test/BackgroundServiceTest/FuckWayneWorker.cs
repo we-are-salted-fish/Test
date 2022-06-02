1c88f4d4a679429ad659648e2497556bac49716b
Bread <ismatch@qq.com>
2022-06-02 14:48:44 +0800

    Fuck Wayne

---
+++ Test/BackgroundServiceTest/FuckWayneWorker.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BackgroundServiceTest
+{
+    public class FuckWayneWorker : BackgroundService
+    {
+        private readonly ILogger<FuckWayneWorker> _logger;
+
+        public FuckWayneWorker(ILogger<FuckWayneWorker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("FuckWayneWorker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+    }
+}
---
+++ Test/BackgroundServiceTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BackgroundServiceTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddHostedService<FuckWayneWorker>();
+            })
+            .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
